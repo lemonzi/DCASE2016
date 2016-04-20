e226680889d17b0a0e0f6386145de32db1269fd9
Quim Llimona <ql@lemonzi.me>
2016-04-20 14:59:02 +0000

    Trim all extended audios to the same length

--- src/extend_dataset.py
+++ src/extend_dataset.py
@@ -1,16 +1,17 @@
+import matplotlib
+matplotlib.use('Agg')
 import librosa
 from numpy import append, split
 
 def extend_dataset(y, sr):
 	# Make 2x faster
 	D       = librosa.stft(y, n_fft=2048, hop_length=512)
-	
+
 	D_fast  = librosa.phase_vocoder(D, 2.0, hop_length=512)
 	y_fast  = librosa.istft(D_fast, hop_length=512)
 
 	# Concatenate two 2x frames together
 	y_fast = append(y_fast, y_fast)
-	y_fast = y_fast[:len(y)]
 
 	# Make 2x slower
 	D_slow  = librosa.phase_vocoder(D, 0.5, hop_length=512)
@@ -18,14 +19,18 @@ def extend_dataset(y, sr):
 
 	# split two 0.5x frames together
 	y_slow1, y_slow2 = split(y_slow, 2)
-	y_slow1 = y_slow1[:len(y)]
-	y_slow2 = y_slow2[:len(y)]
 
 	## Frequency scaling
 	y_pitch_up = librosa.effects.pitch_shift(y, sr, n_steps=4)
 	y_pitch_up = y_pitch_up[:len(y)]
 	#y_pitch_down = librosa.effects.pitch_shift(y, sr, n_steps=-4)
 	#y_pitch_down = y_pitch_down[:len(y)]
+
+        samples = min([len(y), len(y_fast), len(y_slow1), len(y_pitch_up)])
+        y = y[:samples]
+        y_fast = y_fast[:samples]
+        y_slow1 = y_slow1[:samples]
+        y_pitch_up = y_pitch_up[:samples]
 
 	return (y_fast, y_slow1, y_pitch_up)
 
